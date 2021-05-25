c64bf4dd446f629fa97f4ace4ce01dc3c8fd8eb8
HABEEB AWOYEMI <awoyemi.habeeb@gmail.com>
2021-05-25 20:52:23 +0100

    set media query, set color for error and success operation, completed the logic

--- js/index.js
+++ js/index.js
@@ -1,21 +1,44 @@
 const inputValue = document.querySelector('#value');
 const submitBtn = document.querySelector('#btn');
 const result = document.querySelector('#result');
-// const isNumber = true;
 
+//get typeof inputValue
+function getInputType(value) {
+  if(value === "") {
+    return "";
+  }
+  else if(value === "true" || value === "false") {
+    return "boolean";
+  }
+  else if(!Number.isNaN(Number(value))) {
+    return "number";
+  }
+ 
+  
+  else {
+    return typeof(value);
+  }
+}
 
+//function to convert fahr to celcius
 function convertFahrToCelsius(fahrenheit) {
   
   fahrenheit = inputValue.value;
+  inputValue.value = "";
   
   if(!isNaN(fahrenheit)) {
-    let celcius = (fahrenheit - 32) * 5 / 9;
-    return result.innerText = `${celcius.toFixed(4)}`;
+    let celcius = (fahrenheit - 32) * 5 / 9 ;
+    
+    result.style.color = 'green';
+    return result.innerText = `${fahrenheit}degF is equal to ${celcius.toFixed(4)} degC `;
   }
 
   else {
-    return result.innerText = `${fahrenheit} is not a number but ${typeof(fahrenheit)} ` ;
+    result.style.color = 'red';
+    return result.innerText = `${fahrenheit} is not a number but ${getInputType(fahrenheit)} ` ;
   }
+
+  
 } 
 
 submitBtn.addEventListener('click', convertFahrToCelsius);
